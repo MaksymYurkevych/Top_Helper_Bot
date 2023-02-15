ed5e0825a8d55dfa207a246afa69dbd00a834b83
MaksymYurkevych <102907831+MaksymYurkevych@users.noreply.github.com>
2023-02-15 15:56:55 +0200

    Initial commit

---
+++ Top_Helper_Bot/menu.py
@@ -0,0 +1,45 @@
+"""Main Bot menu"""
+
+from my_calc import main as my_calc_main
+from sort import main as sort_main
+from note_functions import run_notes as nb_main
+from ab_functions import main as ab_main
+
+
+def main_menu():
+    work_loop = True
+    while work_loop:
+        user_input = input(
+            '\nBot Main menu\n1 - AdressBook\n2 - NoteBook\n3 - FileSorter\n4 - Calculator\n0 - Exit\n\nBot waiting '
+            '... press button >>> ')
+        if user_input == "1":  # 1 - AdressBook
+            print('Start AdressBook application')
+            ab_main()
+            print('Finish AdressBook application')
+        elif user_input == "2":  # 2 - NoteBook
+            print('Start NoteBook application')
+            nb_main()
+            print('Finish NoteBook application')
+        elif user_input == "3":  # 3 - FileSorter
+            print('Start FileSorter application')
+            sort_main()
+            print('Finish FileSorter application')
+            pass
+        elif user_input == "4":  # 4 - Calculator
+            print('Start Calculator application')
+            my_calc_main()
+            print('Finish Calculator application')
+        elif user_input == 5:
+            pass
+        elif user_input == 6:
+            pass
+        elif user_input == '0':
+            work_loop = False
+        else:
+            continue
+
+    pass
+
+
+if __name__ == '__main__':
+    main_menu()
